7c4012ca8b6b0e620bd493b5c8d18fb6d0d111fa
Sean <1996.sean.alexander@gmail.com>
2020-10-14 19:29:12 -0700

    used 'getServerSideProps()' to pull data on render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,43 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import Header from "../components/Header"
+import { GetServerSideProps } from "next";
+import fetch, {Response } from "node-fetch";
+import { useEffect, useState } from "react";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+library.add(faSearch)
 
-export default IndexPage
+const IndexPage = ({data}) => {
+  // console.log("PROPS", data);
+
+  const [ restaurants, setRestaurants ] = useState(data)
+	return (
+		<Layout title="Home | Next.js + TypeScript Example">
+      <Header />
+			<h1>Hello Next.js 👋</h1>
+		</Layout>
+	);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  let headers: any = {
+    Authorization: process.env.AUTH_KEY
+  }
+	try {
+    const URL: string = process.env.ENDPOINT!;
+    const result: Response = await fetch(URL, {
+      headers: headers,
+    });
+    console.log(typeof result)
+    const data: object = await result.json();
+    console.log(typeof data);
+    return { props: { data } };
+  }
+  catch {
+    return { props: {} }
+  }
+};
+
+export default IndexPage;
