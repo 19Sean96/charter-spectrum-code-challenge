98a476566b76d2ced8ed2df56b6b14377674a595
Sean <1996.sean.alexander@gmail.com>
2020-10-15 07:50:06 -0700

    allocated spacing for each table column. added margin at bottom of table to prevent element hiding

--- components/Table.tsx
+++ components/Table.tsx
@@ -4,23 +4,44 @@ const Table = (props: any) => {
 	const { restaurants } = props;
 	return (
 		<div className="table__wrapper">
-			<div className="table--heading">This is the table heading</div>
-			<Scrollbars
-				style={{ width: "100%", height: "100%" }}
-				universal={true}
-			>
-				{restaurants.map((restaurant: any, index: number) => {
-					return (
-						<div key={index}
-							className={`table--row__wrapper table--row__wrapper--${
-								index % 2 == 0 ? "even" : "odd"
-							}`}
-						>
-							<TableRow  restaurant={restaurant} />
-						</div>
-					);
-				})}
-			</Scrollbars>
+			<div className="table--heading">
+				<div className="table--heading--col table--heading__name">
+					name
+				</div>
+				<div className="table--heading--col table--heading__city">
+					city
+				</div>
+				<div className="table--heading--col table--heading__state">
+					state
+				</div>
+				<div className="table--heading--col table--heading__phone">
+					phone #
+				</div>
+				<div className="table--heading--col table--heading__genres">
+					genres
+				</div>
+				<span className="table--heading--underline"></span>
+			</div>
+			<div className="table--data">
+				<Scrollbars
+					style={{ width: "100%", height: "100%" }}
+					universal={true}
+				>
+					{restaurants.map((restaurant: any, index: number) => {
+						return (
+							<div
+								key={index}
+								className={`table--row__wrapper table--row__wrapper--${
+									index % 2 == 0 ? "even" : "odd"
+								}`}
+							>
+								<TableRow restaurant={restaurant} />
+							</div>
+						);
+					})}
+					<span></span>
+				</Scrollbars>
+			</div>
 		</div>
 	);
 };
--- components/TableRow.tsx
+++ components/TableRow.tsx
@@ -1,8 +1,21 @@
-const TableRow = (props: any) => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const TableRow = (props: any) => {
+    const { restaurant } = props;
     return (
         <article className="table--row">
-            <p>{props.restaurant.name}</p>
+            <div className="table--row--main">
+                <p className="table--row__name">{restaurant.name}</p>
+                <p className="table--row__city">{restaurant.city}</p>
+                <p className="table--row__state">{restaurant.state}</p>
+                <p className="table--row__phone">{restaurant.telephone}</p>
+                <p className="table--row__genres">{restaurant.genre}</p>
+            </div>
+            <div className="table--row--more"></div>
+
+            <button className="table--row--expand">
+                <FontAwesomeIcon icon="angle-double-down" />
+            </button>
         </article>
     )
 }
